c6bb9b8a1cef525effb802b203c4454585239aaf
Emil Bjørn <bjoern.emil@gmail.com>
2019-10-27 17:10:38 +0100

    init

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ let windowHeight = 600;
 let n = 3;
 let scale = 200 / n;
 let ang = 10;
+let mils;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,16 +13,18 @@ function setup() {
 function draw() {
   translate(canvas.width / 2, canvas.height / 2);
   background(100);
-  fill(255);
+  fill(200);
   // initialize(10);
-  for (let index = 0; index < n; index++) {
-    // push();
+  ellipse(0, 0, scale);
+  fill(255);
+  for (let index = 1; index < n; index++) {
+    push();
     rotate(index * ang);
     ellipse(0, -index * scale, scale, scale);
-    // pop();
+    pop();
   }
 
-  ang += 0.01;
+  ang += 1;
 }
 
 // function initialize(n) {
